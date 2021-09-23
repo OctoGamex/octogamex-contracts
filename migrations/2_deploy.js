8ba7ed58b17aaef0a9416c5e8713846d90945989
Andrii Loborchuk <loborchuka@elephantslab.com>
2021-09-23 11:18:21 +0300

    Added comission parametr

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -2,6 +2,6 @@ const Market = artifacts.require("NFT_Market");
 const ERC = artifacts.require("TestERC");
 
 module.exports = function (deployer) {
-  deployer.deploy(Market);
+  deployer.deploy(Market, 5);
   deployer.deploy(ERC);
 };
