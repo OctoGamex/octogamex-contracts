d506df8fbca725551d3a0aa694598ce6e8d67c16
Andrii Loborchuk <loborchuka@elephantslab.com>
2021-09-23 18:06:50 +0300

    Added offer comission, added owners for lots, proposal, created make_offer

--- contracts/NFT_Market.sol
+++ contracts/NFT_Market.sol
@@ -8,6 +8,7 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 contract NFT_Market is Ownable{
 
     uint8 immutable public m_comission;
+    uint8 public prop_comission;
 
     lot_info [] public lots; 
     proposal [] public proposals;
@@ -41,13 +42,14 @@ contract NFT_Market is Ownable{
     }
 
     struct currency{
-        uint256 id_contract;
+        address id_contract;
         uint256 seller_price;
         uint256 buyer_price;
     }
 
     struct proposal{
-        lot_info [] nft_props;
+        address owner;
+        uint256 [] lots_prop;
         currency crypto_proposal;
     }
 
@@ -90,21 +92,98 @@ contract NFT_Market is Ownable{
         delete lots[index];
         ERC1155 nft_contract = ERC1155(lot.contract_add);
         nft_contract.safeTransferFrom(address(this), msg.sender, lot.id, lot.amount, data_);
-        payable(lot.owner).send(lot.seller_price);
+        payable(lot.owner).transfer(lot.seller_price);
         emit Buy(lot.contract_add, lot.id, msg.sender, lot.amount, lot.buyer_price);
     }
 
-    // function make_offer(uint256 [] lot_index, uint256 payment_index, uint256 payment, bytes [] data_) external payable {
-    //     for (i=0; i< lot_index.length; i++){
-    //         require(lot_owner[i] == msg.sender, "You are not the owner");
+    function make_offer (uint256 index, uint256 [] memory lot_index, uint256 token_index, uint256 payment, bytes [] memory data_) external payable {
+        require(lots[index].owner != msg.sender, "You are owner" );
+        if (msg.value == prop_comission && lot_index.length == 0 && payment != 0){ //only tokens
+            proposals.push(
+                proposal(
+                    msg.sender,
+                    lot_index,
+                    currency(    
+                        erc20_address[token_index],                
+                        payment - payment * m_comission / 100,
+                        payment
+                    )
+            ));
+            proposal_owner[msg.sender].push(proposals.length - 1);
+        } else if(msg.value > prop_comission && lot_index.length != 0 && payment == 0) {  //crypto and nft with payment
+            for (uint256 i=0; i < lot_index.length; i++){
+                require(lots[i].owner == msg.sender, "You are not the owner");
+            }
+            proposals.push(
+                proposal(
+                    msg.sender,
+                    lot_index,
+                    currency(    
+                        address(0),                
+                        msg.value - msg.value * m_comission / 100,
+                        msg.value
+                    )
+            ));
+            proposal_owner[msg.sender].push(proposals.length - 1);
+        } else if (msg.value == prop_comission && lot_index.length != 0 && payment == 0) { // nft
+            for (uint256 i=0; i < lot_index.length; i++){
+                require(lots[i].owner == msg.sender, "You are not the owner");
+            }
+            proposals.push(
+                proposal(
+                    msg.sender,
+                    lot_index,
+                    currency(    
+                        address(0),                
+                        0,
+                        0
+                    )
+            ));
+            proposal_owner[msg.sender].push(proposals.length - 1);
+        } else if (msg.value > prop_comission && lot_index.length == 0 && payment == 0) { // crypto
+            proposals.push(
+                proposal(
+                    msg.sender,
+                    lot_index,
+                    currency(    
+                        address(0),                
+                        msg.value - msg.value * m_comission / 100,
+                        msg.value
+                    )
+            ));
+            proposal_owner[msg.sender].push(proposals.length - 1);
+        } else if (msg.value == prop_comission && lot_index.length != 0 && payment != 0) { // nft + token  
+            for (uint256 i=0; i < lot_index.length; i++){
+                require(lots[i].owner == msg.sender, "You are not the owner");
+            }
+            proposals.push(
+                proposal(
+                    msg.sender,
+                    lot_index,
+                    currency(    
+                        erc20_address[token_index],                
+                        payment - payment * m_comission / 100,
+                        payment
+                    )
+            ));
+            proposal_owner[msg.sender].push(proposals.length - 1);
+        } else {
+            revert("Wrong parametr");
+        }
+    }
+
+    // function cancel_offer (uint256 index) external {
+    //     require(proposals[index].owner == msg.sender, "You are not the owner!" );
+    //     proposal l_proposal = proposals[index];
+    //     if (l_proposal.nft_props.length == 0) {
+
+    //     } else {
 
     //     }
-    //     if (msg.value == 0){
-    //         uint256 b = payment;
-    //         throw;
-    //     } else {
-    //         proposal new_prop = proposal();
-    //     }
+    // }
+
+    // function get_lots(uint256 [] indexes) external view returns(lot_info []) {
+    //     for (uint256 i = 0; i < indexes.length, )
     // }
 
     function onERC1155Received(
