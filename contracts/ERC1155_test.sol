319d8d8286be9ddbabfb2f03323599652f7c1a5b
Andrii Loborchuk <loborchuka@elephantslab.com>
2021-09-24 13:23:38 +0300

    changed contract name

--- contracts/ERC1155_test.sol
+++ contracts/ERC1155_test.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-contract TestERC is ERC1155{
+contract TestERC1155 is ERC1155{
 
   constructor () ERC1155('asd') {
 
