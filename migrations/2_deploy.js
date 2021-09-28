336042b5b95bf7a87c9c0807a0ddfe65fda78ff4
Andrii Loborchuk <loborchuka@elephantslab.com>
2021-09-28 12:32:54 +0300

    Added market wallet

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -3,7 +3,7 @@ const ERC1155 = artifacts.require("TestERC1155");
 const ERC20 = artifacts.require("TestERC20");
 
 module.exports = function (deployer) {
-  deployer.deploy(Market, 5, 2);
+  deployer.deploy(Market, 5, 2, "0x56C49D8B396E7cd600e558B1e1Fa43853A306BF9");
   deployer.deploy(ERC1155);
   deployer.deploy(ERC20);
 };
