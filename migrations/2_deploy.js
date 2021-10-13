3ca1dee5fc098d2d3e10b10553625644ee10bf48
Andrii Loborchuk <loborchuka@elephantslab.com>
2021-10-13 12:39:26 +0300

    Rename contract artifacts

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,9 +1,9 @@
-const Market = artifacts.require("NFT_Market");
+const Market = artifacts.require("NFTMarketplace");
 const ERC1155 = artifacts.require("TestERC1155");
 const ERC20 = artifacts.require("TestERC20");
 const my_ether = web3.utils.toWei('1', "ether");
 module.exports = function (deployer) {
-  deployer.deploy(Market, 90, (my_ether/20).toString(), "0x09B61CA66838a02e3555a837EefA816598ebAc93");
+  deployer.deploy(Market, 100, (my_ether/20).toString(), "0x09B61CA66838a02e3555a837EefA816598ebAc93");
   deployer.deploy(ERC1155);
   deployer.deploy(ERC20);
 };
