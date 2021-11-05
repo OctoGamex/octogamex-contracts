3c4032902bb7b13638e1749f62055c3c2f789283
Andrii Loborchuk <loborchuka@elephantslab.com>
2021-11-05 15:01:26 +0200

    Added deploy Auction

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -2,10 +2,13 @@ const Market = artifacts.require("NFTMarketplace");
 const ERC1155 = artifacts.require("TestERC1155");
 const ERC20 = artifacts.require("TestERC20");
 const ERC721 = artifacts.require("TestERC721");
+const Auction = artifacts.require("Auction");
 const my_ether = web3.utils.toWei('1', "ether");
-module.exports = function (deployer) {
-  deployer.deploy(Market, 100, (my_ether/20).toString(), "0x09B61CA66838a02e3555a837EefA816598ebAc93");
-  deployer.deploy(ERC1155);
-  deployer.deploy(ERC20);
-  deployer.deploy(ERC721);
+
+module.exports = async function (deployer) {
+  await deployer.deploy(Market, 100, (my_ether/20).toString(), "0x09B61CA66838a02e3555a837EefA816598ebAc93");
+  await deployer.deploy(Auction, Market.address)
+  await deployer.deploy(ERC1155);
+  await deployer.deploy(ERC20);
+  await deployer.deploy(ERC721);
 };
